d82dcee77532855bc950da8abfe579033d80e325
Andreas W <windemut@gmail.com>
2013-07-02 17:32:54 -0400

    First two programming assignments

---
+++ programs/primes.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "primes.txt";
+
+function primes(ps, n) {
+  if (ps.length>=100) return ps;   // got 100 primes, we are done
+  else {
+    if (ps.
+      filter(function(i){return i*i<=n}). // check all potential factors
+      some(function(i){return n%i==0})    // if any of them divide,
+    ) return primes(ps, n+1);             // not prime, try next
+    else return primes(ps.concat([n]), n+1); // otherwise, add to list.
+  }
+}
+
+var out = primes([], 2)
+console.log("Primes: " + out);
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+
